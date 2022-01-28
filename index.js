16548205bd4e0247c422a8fdb170f96cbc29186a
geddy89 <generalgeddy@yahoo.com>
2022-01-27 20:09:42 -0800

    commit tree 696870fcd32602e9882140e43c38c3610b0e04f6

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+/*var num1 = 31;
 
-// Write your code here
+var num2 = 2;
+multiply(num1, num2);
+multiply(3412, 6969);
+multiply(420, 566);
+function multiply(pizza, hotDog) {
+answer = pizza * hotDog;
+console.log("pizza value ",pizza)
+console.log(answer)
+}
+ doesnt please the test     */ 
+
+//let multiply = 31 * 2
+
+const multiply = 62;
+
+var num1 = 31;
+
+var num2 = 2;
+
+const random = Number.isInteger(Math.random() * 10) + 1;
+
+var num3 = 20;
+
+var num4 = 5;
+
+const mod = 4;
+
+const max = 20
